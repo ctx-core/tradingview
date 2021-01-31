ba6496738e78681504722cc661b1a023765847cb
Brian Takita <brian.takita@gmail.com>
2021-01-31 05:25:34 -0500

    update dependencies

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # @ctx-core/tradingview
 
+## 6.0.0
+
+### Major Changes
+
+- src directory
+
 ## 5.0.348
 
 ### Patch Changes
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import Chart__TradingView from './Chart__TradingView.svelte'
+import MarketOverview__TradingView from './MarketOverview__TradingView.svelte'
+import MediumWidget__TradingView from './MediumWidget__TradingView.svelte'
+import MiniSymbolOverview__TradingView from './MiniSymbolOverview__TradingView.svelte'
+export {
+	Chart__TradingView,
+	MarketOverview__TradingView,
+	MediumWidget__TradingView,
+	MiniSymbolOverview__TradingView
+}
